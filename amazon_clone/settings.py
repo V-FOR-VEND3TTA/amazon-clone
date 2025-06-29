f1150dcf0f90bbfb878797758ca66ea997a24fbd
George Hlongwane <georgehlongwane8@gmail.com>
2025-06-30 00:47:46 +0200

    Configured installed apps in settings

--- amazon_clone/settings.py
+++ amazon_clone/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom installed apps
+    'cart',
+    'core',
+    'orders',
+    'users',
 ]
 
 MIDDLEWARE = [
