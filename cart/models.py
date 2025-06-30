1098fdab8a8a2db2025af2661be24c185e2faeb5
George Hlongwane <georgehlongwane8@gmail.com>
2025-06-30 21:38:47 +0200

    Created a persistent cart model for the cart app

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from core.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
